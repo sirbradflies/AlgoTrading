5d0315d20128b1a9538f663d9b438fa6cdc68313
Francesco Baldisserri <fbaldisserri@gmail.com>
2020-01-30 13:16:55 +0100

    Work in progress with multi features and encoder

--- machine_learning.py
+++ machine_learning.py
@@ -1,32 +1,29 @@
-import os
-import requests
+import random
 import pandas as pd
 import backtrader as bt
-from datetime import datetime
+from datetime import datetime as dt
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.neural_network import MLPRegressor
-from iexfinance.stocks import get_historical_data
-# TODO: Explore why there are peaks of short stocks
+from yahoofinancials import YahooFinancials as yf
 
 
 class MachineLearning(bt.Strategy):
-    params = dict(long_stocks=2,
-                  short_stocks=2)
+    params = dict(long_stocks=3, short_stocks=3)
 
-    def __init__(self):  # TODO: Add position (long / short) indicator
-        self.model = MLPRegressor(warm_start=True)
+    def __init__(self, model, encoder, features):
         self.order_target = 1.0/(self.p.long_stocks+self.p.short_stocks)
-        self.pred_return = {d._name: PredictedReturn(d, model=self.model, plot=False)
-                            for d in self.datas}
-        # TODO: Add nr. of trades per tick
+        self.pred_return = {}
+        for d in self.datas:
+            self.pred_return[d._name] = PredictedReturn(
+                d, model=model, encoder=encoder, plot=False, features=features
+            )
 
     def next(self):
         stock_returns = self.get_stocks_pred_return()
-        # TODO: Change when New_Pred_Return > Old_Pred_Return + Commissions
+        # TODO: Include commissions friction in pred_return
         long_stocks = stock_returns.head(self.p.long_stocks).index.values
         short_stocks = stock_returns.tail(self.p.short_stocks).index.values
-        # TODO: Check balancing strategy on: https://www.backtrader.com/blog/2019-07-19-rebalancing-conservative/rebalancing-conservative/
         for d in self.datas:
-            self.log_position(d)
             if d._name in long_stocks:
                 self.order_target_percent(data=d, target=self.order_target)
             elif d._name in short_stocks:
@@ -35,12 +32,7 @@ class MachineLearning(bt.Strategy):
                 self.order_target_percent(data=d, target=0.0)
 
     def notify_order(self, order):
-        # Check if an order has been completed
-        if order.status in [order.Completed]:
-            self.log_order(order)
-
-        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
-            self.log('Order Canceled/Margin/Rejected')
+        self.log_order(order)
 
     def notify_trade(self, trade):
         if trade.isclosed:
@@ -49,12 +41,9 @@ class MachineLearning(bt.Strategy):
 
     def log_position(self, datafeed):
         """ Return position value of a datafeed """
+        position = self.positions[datafeed]
         self.log(f'Position: {datafeed._name}\t'
-                 f'Value: {self.get_position_value(datafeed)}')
-
-    def get_position_value(self, datafeed):
-        """ Return the total value of a position """
-        return self.positions[datafeed].price*self.positions[datafeed].size
+                 f'Value: {position.price * position.size:,.0f}')
 
     def get_stocks_pred_return(self):
         """ Return a dataframe with the predicted return for all stocks """
@@ -65,7 +54,8 @@ class MachineLearning(bt.Strategy):
 
     def log_order(self, order):
         """ Log order price, cost and commissions """
-        msg = 'Sell' if order.issell() else 'Buy'
+        msg = f'Order status is {order.Status[order.status]} - '
+        msg += 'Sell' if order.issell() else 'Buy'
         self.log(f'{msg} {order.params.data._name}, '
                  f'Price: {order.executed.price:.2f}, '
                  f'Cost: {order.executed.value:.2f}, '
@@ -79,87 +69,106 @@ class MachineLearning(bt.Strategy):
 
 class PredictedReturn(bt.Indicator):
     lines = ('pred_return',)
+    params = dict(min_datapoints=10)
 
-    def __init__(self, model, lookback=12):
+    def __init__(self, model, encoder, features, lookback=60):
         self.model = model
+        self.encoder = encoder
+        self.features = features
         self.lookback = lookback
-        self.addminperiod(self.lookback + 1)
+        self.addminperiod(self.lookback + self.p.min_datapoints)
         self.lines.actual_return = self.data_close / self.data_close(-1) - 1
 
     def next(self):  # TODO: Make it declarative
         self.train_model()
         self.lines.pred_return[0] = self.model.predict([self.get_features(ago=0)])/self.data_close[-1]-1
 
-    def train_model(self):
-        X, y = [], []
+    def train_model(self):  # TODO: Move train model to strategy
+        features, target = [], []
         datapoints = len(self.data) - self.lookback
         for c in range(0, datapoints):  # TODO: Limit datapoints for efficiency
-            X += [self.get_features(ago=-c-1)]
-            #y += self.lines.actual_return.get(ago=-c)
-            y += self.data_close.get(ago=-c)
-        self.model.fit(X, y)  # TODO: Fix negative score
-        print(f'Model score for {self.data._name}: {self.model.score(X, y)}')
+            features += [self.get_features(ago=-c-1)]
+            target += self.data.adjclose.get(ago=-c)
+        #X = self.encoder.transform(features)
+        X = features
+        self.model.fit(X, target)
+        print(f'Model score for {self.data._name}: '
+              f'{self.model.score(X, target):.4f}\t({datapoints} pts.)')
 
     def get_features(self, ago=0):
-        close = list(self.data_close.get(ago=ago, size=self.lookback))
-        #volume = list(self.data_volume.get(ago=ago, size=self.lookback))  # TODO: Add back volume
-        return close
+        feature_data = []
+        for feature in self.data.features:
+            feature_line = getattr(self.data,feature)
+            feature_data = feature_line.get(ago=ago, size=self.lookback)
+        return feature_data
 
 
-def set_iex_mode(mode='sandbox'):
-    """ Select IEX mode: 'sandbox' or 'live' """
-    if mode == 'sandbox':
-        os.environ['IEX_API_VERSION'] = 'iexcloud-sandbox'
-        os.environ['IEX_TOKEN'] = 'Tpk_4cd09bd0d413453e855bb465198b29ca'
-    elif mode == 'live':
-        os.environ['IEX_API_VERSION'] = 'iexcloud-sandbox'
-        os.environ['IEX_TOKEN'] = 'pk_760e9e753a004c6f9f45088c0d7cc42f'
+class CustomBroker(bt.Observer):
+    """ Tailored broker to observe long short positions and trades """
+    lines = ('long_short', 'value')
+    plotinfo = dict(plot=True, subplot=True)
+
+    def next(self):
+        self.lines.value[0] = self._owner.broker.getvalue()
+        self.lines.long_short[0] = self.get_positions_values()
+
+    def get_positions_values(self):
+        """ Sum total value of all open positions """
+        positions_value = 0
+        for feed in self._owner.broker.positions:
+            position = self._owner.broker.positions[feed]
+            positions_value += position.price * position.size
+        return positions_value
 
 
 def get_sp500_symbols():
+    #TODO: Survivorship bias free listing
+    # https://www.quora.com/How-can-I-get-the-complete-list-survivorship-bias-free-of-symbols-in-S-P-500
     """ Get S&P500 actualized list of symbols from wikipedia """
     table = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
-    return table[0]['Symbol']
+    return list(table[0]['Symbol'])
 
 
-def get_historic_prices(stock, start, end):
-    """ Get historical prices from IEX data provider """
-    return get_historical_data(stock, start, end,
-                               output_format='pandas')
+def get_yahoo_prices(symbol, start, end, frequency='daily'):
+    """ Get historic prices in a dataframe from Yahoo Finance """
+    prices = yf(symbol.replace('.','-')).get_historical_price_data(
+        start_date=start, end_date=end, time_interval=frequency
+    )
+    if not ('prices' in prices[symbol]
+            and 'date' in prices[symbol]['prices'][0]):
+        print(f'Keys missing for {symbol}!!!')
+    df = pd.DataFrame(prices[symbol]['prices'])
+    return df.set_index(df['date'].apply(dt.fromtimestamp))
 
 
-def download_yahoo_stocks(stock, start='1262304000', end='1577836800', crumb='GwEq4sk1BX9'):
-    r = requests.get(
-        f"https://query1.finance.yahoo.com/v7/finance/download/{stock}?period1={start}&period2={end}&interval=1d&events=history&crumb={crumb}")
-    file = open(f"data\{stock}.csv", 'w')
-    file.write(r.text)
-    file.close()
+class CustomFeed(bt.feeds.PandasData):
+    features = ['adjclose', 'open', 'close']
+    lines = tuple(features)
+    params = {feature: -1 for feature in features}
 
 
 if __name__ == '__main__':
     cerebro = bt.Cerebro()
     cerebro.broker.setcash(100000.0)
-    cerebro.broker.setcommission(commission=0.01)  # TODO: Set commissions
-    #stock_universe = get_sp500_symbols().sample(10)
-    for symbol in get_sp500_symbols():
-        download_yahoo_stocks(symbol)
-    #set_iex_mode('sandbox')
-    stock_universe = [f for f in os.listdir('data/') if '.csv' in f]
-    for stock in stock_universe:
-        #iex_prices = get_historic_prices(stock=stock,
-        #                                 start=datetime(2018, 1, 1),
-        #                                 end=datetime(2020, 1, 1))
-        #datafeed = bt.feeds.PandasData(dataname=iex_prices,
-        #                               name=stock,
-        #                               openinterest=None)
-        datafeed = bt.feeds.YahooFinanceCSVData(dataname=f'data/{stock}',
-                                                fromdate=datetime(2010, 1, 1),
-                                                todate=datetime(2020, 1, 1),
-                                                plot=False)
-        cerebro.resampledata(datafeed, timeframe=bt.TimeFrame.Days, compression=1)
-    cerebro.addstrategy(MachineLearning)
+    cerebro.broker.setcommission(commission=0.001)
+    stock_universe = random.sample(get_sp500_symbols(), 10)  # TODO: Select stocks public at the start date?
+    encoder = MinMaxScaler()
+    for symbol in stock_universe:  # TODO: Add fundamentals: https://www.backtrader.com/blog/2019-07-19-rebalancing-conservative/rebalancing-conservative/
+        print(f'Preparing feed for {symbol}')
+        df_prices = get_yahoo_prices(symbol,
+                                     start='2010-01-01',
+                                     end='2020-01-01',
+                                     frequency='weekly')
+        #encoder.partial_fit(df_prices[CustomFeed.features])
+        datafeed = CustomFeed(dataname=df_prices, name=symbol, plot=False)
+        cerebro.adddata(datafeed)
+    cerebro.addstrategy(MachineLearning,
+                        model=MLPRegressor(warm_start=True),
+                        encoder=encoder,
+                        features=CustomFeed.features)
+    cerebro.addobserver(CustomBroker)
     print(f'Starting Portfolio Value: {cerebro.broker.getvalue():.2f}')
-    for analyzer in [bt.analyzers.SharpeRatio, bt.analyzers.Returns, bt.analyzers.DrawDown]:
+    for analyzer in [bt.analyzers.SharpeRatio, bt.analyzers.Returns, bt.analyzers.DrawDown]:  # TODO: Add Sortino
         cerebro.addanalyzer(analyzer)
     results = cerebro.run()
     print(f'Ending Portfolio Value: {cerebro.broker.getvalue():.2f}')
