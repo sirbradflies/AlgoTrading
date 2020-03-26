ff6ef47e3294b7e11afc48bc7d59866a23e99c73
Francesco Baldisserri <fbaldisserri@gmail.com>
2020-03-26 17:39:40 +0100

    Working version to be refactored

--- QC_Machine_Learning.py
+++ QC_Machine_Learning.py
@@ -1,9 +1,9 @@
 """
-Basic Machine Learning bot for Quantconnect
+Machine Learning Value bot for Quantconnect
 
 @author: Francesco Baldisserri
 @email: fbaldisserri@gmail.com
-@version: 0.1
+@version: 0.3
 """
 
 import clr
@@ -16,69 +16,82 @@ from QuantConnect import *
 from QuantConnect.Algorithm import *
 
 import pandas as pd
+from datetime import datetime
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import train_test_split
 
 
 class NeuralNetworkAlgorithm(QCAlgorithm):
     def Initialize(self):
-        self.SetStartDate(2018, 7, 1)
+        self.SetStartDate(2008, 7, 1)
         self.SetEndDate(2019, 6, 30)
-        self.lookback = 252
         self.portfolio_stocks = 20
         self.long_short_ratio = 0.5  # 1.0 Long only <-> 0.0 Short only
         self.long_pos = int(self.portfolio_stocks * self.long_short_ratio)
         self.short_pos = self.portfolio_stocks - self.long_pos
-        self.model = MLPRegressor()
-        self.rebalance = False
-        self.month = -1
+        self.feat_encoder, self.targ_encoder = None, None
+        self.model = MLPRegressor(hidden_layer_sizes=(64, 32), tol=0,
+                                  early_stopping=True, warm_start=True)
+        self.history = pd.DataFrame()
+        self.history_maxlen = 10000  # TODO: Convert to periods
+        self.last_update = self.last_execution = datetime(1, 1, 1)
         self.AddUniverse(self.top_fundamentals, self.store_fundamentals)
-        self.data_table = pd.DataFrame()
+        self.Schedule.On(self.DateRules.EveryDay(),
+                         self.TimeRules.At(10, 0, 0),
+                         self.execute_strategy)
 
-    def OnData(self, data):  # TODO: Encode variables
+    def execute_strategy(self):
         """ Prepare the data, train the ML model and trade """
-        if self.rebalance:
+        active_stocks = [s for s in list(self.ActiveSecurities.Keys)
+                         if self.IsMarketOpen(s)]
+        if self.last_update > self.last_execution \
+                and len(active_stocks) > self.portfolio_stocks:  # New data, prepare features
+            self.last_execution = self.last_update
             features, targets = self.get_data(training=True)
-            active_stocks = [s for s in list(self.ActiveSecurities.Keys)
-                             if self.IsMarketOpen(s)]
-            if len(features) > 0 and len(active_stocks) > self.portfolio_stocks:
+            if len(features) > 0:  # Enough features, train model
                 score = self.train_model(features, targets)
                 self.Debug(f'Time: {self.Time}\tPoints: {len(features)}\t'
                            f'Epochs: {self.model.n_iter_}\tScore: {score:.4f}')
-                if score > 0:
+                if score > 0:  # Valid model, make predictions and trade
                     features, _ = self.get_data(training=False, symbols=active_stocks)
                     pred_returns = self.predict_returns(features)
                     self.trade(pred_returns, score)
-            self.rebalance = False
 
-    def get_data(self, lookback=1, training=True, symbols=None):
+    def get_data(self, training=True, symbols=None):  # TODO: Test with lookback periods
         """ Return features and target both for training and prediction """
-        data = self.data_table.unstack('symbol')
-        data['target'] = data['target'].shift(-lookback, axis='index')
-        data = data.stack('symbol')
+        data = self.history.dropna()
         if symbols is not None:
             data = data[data.index.get_level_values('symbol').isin(symbols)]
-        data = data.dropna() if training else data[data['target'].isnull()]
-        targets = data.pop('target')
+        target = data['return'].unstack('symbol').shift(-1).stack('symbol', dropna=False)
+        target = target.dropna() if training else target.loc[target.isnull()]  
         features = data
-        return features, targets
+        mask = target.index.intersection(features.index)  # TODO: Refactor
+        return features.loc[mask, :], target.loc[mask]
 
-    def train_model(self, features, targets):
+    def train_model(self, features, target):
         """ Train model with passed data and return validation score """
-        X_train, X_val, Y_train, Y_val = train_test_split(features, targets)
+        if self.feat_encoder is None or self.targ_encoder is None:
+            self.feat_encoder = MinMaxScaler().fit(features)
+            self.targ_encoder = MinMaxScaler.fit(target.values.reshape(-1, 1))
+        X = self.feat_encoder.transform(features)
+        Y = self.targ_encoder.transform(target.values.reshape(-1, 1))
+        X_train, X_val, Y_train, Y_val = train_test_split(X, Y)
         self.model.fit(X_train, Y_train)
         return self.model.score(X_val, Y_val)
 
     def predict_returns(self, features):
         """ Return expected returns by symbol """
-        Y = self.model.predict(features)
-        return pd.DataFrame(Y, index=features.index.get_level_values('symbol'))
+        X = self.targ_encoder.transform(features)
+        Y = self.model.predict(X).reshape(-1, 1)
+        return pd.DataFrame(self.targ_encoder.inverse_transform(Y),
+                            index=features.index.get_level_values('symbol'))
 
-    def trade(self, returns, score):
+    def trade(self, returns, score):  # TODO: Clean
         """ Rank returns and select the top for long and bottom for short """
-        to_long = self.rank_stocks(returns, long=True, score=score)\
+        to_long = self.rank_stocks(returns, long=True, confidence=score)\
             .head(self.long_pos).index
-        to_short = self.rank_stocks(returns, long=False, score=score)\
+        to_short = self.rank_stocks(returns, long=False, confidence=score)\
             .head(self.short_pos).index
         invested = [s for s in self.Securities.Keys if self.Portfolio[s].Invested]
         to_sell = set(invested) - set(to_long) - set(to_short)
@@ -91,15 +104,15 @@ class NeuralNetworkAlgorithm(QCAlgorithm):
         self.Log(f'Buy stocks: {to_long}\nSell stocks: {to_short}')
         self.Log(f'Portfolio changes: {len(to_sell)}/{len(invested)}')
 
-    def rank_stocks(self, pred_returns, long=True, commissions_pct=0.01, score=1):
+    def rank_stocks(self, pred_returns, long=True, commission=0.01, confidence=1):
         """
         Calculate best stocks to long or short from predicted returns
         and accounting for commissions and current portfolio positions
         """
-        friction = (-1 if long else +1) * commissions_pct
+        friction = (-1 if long else +1) * commission
         ranking = {}
         for symbol, row in pred_returns.iterrows():
-            exp_return = row[0] * max(score, 0)  # Normalizing returns according to model score
+            exp_return = row[0] * max(confidence, 0)  # Normalizing returns according to model score
             position = self.Portfolio[symbol]
             if (long and position.IsLong) or (not long and position.IsShort):  # Symbol already in the position desired
                 ranking[symbol] = exp_return  # No commissions
@@ -112,11 +125,10 @@ class NeuralNetworkAlgorithm(QCAlgorithm):
 
     def top_fundamentals(self, coarse):
         """ Return top 1000 stocks by volume with fundamentals """
-        if self.month == self.Time.month:
+        if self.last_update.month == self.Time.month:
             return Universe.Unchanged
         else:
-            self.rebalance = True
-            self.month = self.Time.month
+            self.last_update = self.Time
             ranked_stocks = sorted([x for x in coarse if x.HasFundamentalData],
                                    key=lambda x: x.DollarVolume, reverse=True)
             return [x.Symbol for x in ranked_stocks[:1000]]
@@ -127,15 +139,16 @@ class NeuralNetworkAlgorithm(QCAlgorithm):
         for x in fine:
             rows += [{'time': self.Time,
                       'symbol': x.Symbol,
-                      'mom': x.ValuationRatios.PriceChange1M,
                       'pe': x.ValuationRatios.PERatio,
                       'pb': x.ValuationRatios.PBRatio,
                       'pcf': x.ValuationRatios.PCFRatio,
                       'ni': x.OperationRatios.NetMargin.OneYear,
                       'roa': x.OperationRatios.ROA.OneYear,
-                      'ae': x.OperationRatios.FinancialLeverage.OneYear,
-                      'target': x.ValuationRatios.PriceChange1M}]  # TODO: Correct assets/equity?
+                      'ae': x.OperationRatios.FinancialLeverage.OneYear,  # TODO: Correct assets/equity?
+                      'return': x.ValuationRatios.PriceChange1M}]
         new_data = pd.DataFrame(rows).drop_duplicates(['time', 'symbol'])
         new_data = new_data.set_index(['time', 'symbol'])
-        self.data_table = self.data_table.append(new_data)
+        self.history = self.history.append(new_data)
+        self.history = self.history.tail(self.history_maxlen) \
+            if len(self.history) > self.history_maxlen else self.history
         return [x.Symbol for x in fine]
